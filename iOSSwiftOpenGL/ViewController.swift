9b049a03d0b387593ae6415d1c67391d66da01a2
bradley <bradley.j.griffith@gmail.com>
2014-06-29 20:02:56 -0500

    Initial Commit

---
+++ iOSSwiftOpenGL/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSSwiftOpenGL
+//
+//  Created by Bradley Griffith on 6/29/14.
+//  Copyright (c) 2014 Bradley Griffith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
